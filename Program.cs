f4464f86231bcac566d2e4884865e9c94a35002f
Kalai_2216 <144314801+Kalai-2216@users.noreply.github.com>
2025-06-26 15:32:53 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,22 @@
 ﻿using System;
 
-class Company
+class marks
+{
+    static void Main(String[] args)
+    {
+        Console.Write("Enter the name : ");
+        string name = Console.ReadLine();
+        Console.Write("Enter the number of Subjects :");
+        int subjects=Convert.ToInt32(Console.ReadLine());
+        int total = 0;
+        for(int i = 0; i < subjects; i++)
+        {
+            Console.Write($"Enter the Subject {i+1} marks : ");
+            int marks=Convert.ToInt32(Console.ReadLine());
+            total += marks;
+        }
 
+        Console.WriteLine($"{name} has gained {total} marks.");
+
+    }
+}
